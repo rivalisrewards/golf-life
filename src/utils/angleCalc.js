55fb2c2a185fb38299166c48bf7e2a29926d30ac
rivalisrewards <rivalisrewards@gmail.com>
2026-03-17 20:52:15 -0700

    Create angleCalc.js

---
+++ src/utils/angleCalc.js
@@ -0,0 +1,11 @@
+export function getAngle(a, b, c) {
+  const ab = { x: a.x - b.x, y: a.y - b.y };
+  const cb = { x: c.x - b.x, y: c.y - b.y };
+
+  const dot = ab.x * cb.x + ab.y * cb.y;
+  const magAB = Math.sqrt(ab.x ** 2 + ab.y ** 2);
+  const magCB = Math.sqrt(cb.x ** 2 + cb.y ** 2);
+
+  const angle = Math.acos(dot / (magAB * magCB));
+  return (angle * 180) / Math.PI;
+}
